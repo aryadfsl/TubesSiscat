91453f47969ebe13296f4bd060be4baf14318f21
Arya <muhamad.arya@mhs.itenas.ac.id>
2025-01-10 16:52:57 +0700

    buat baru

---
+++ SiscatTubesOop/src/main/java/com/mycompany/siscattubesoop/SiscatTubesOop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.siscattubesoop;
+
+/**
+ *
+ * @author ASUS
+ */
+public class SiscatTubesOop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
