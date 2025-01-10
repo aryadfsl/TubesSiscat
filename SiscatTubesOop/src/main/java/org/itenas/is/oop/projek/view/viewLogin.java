933a067b3ebef9a86c34a0f470280b09bd329b61
Arya <muhamad.arya@mhs.itenas.ac.id>
2025-01-10 18:13:16 +0700

    menyelesaikan semua fitur di TampilanUtama di package view

--- SiscatTubesOop/src/main/java/org/itenas/is/oop/projek/view/viewLogin.java
+++ SiscatTubesOop/src/main/java/org/itenas/is/oop/projek/view/viewLogin.java
@@ -10,7 +10,7 @@ import org.itenas.is.oop.projek.repository.PengelolaLogin;
 
 /**
  *
- * @author Ahmad
+ * @author ASUS
  */
 public class viewLogin extends javax.swing.JFrame {
 
@@ -125,11 +125,8 @@ public class viewLogin extends javax.swing.JFrame {
         jLabel1.setForeground(new java.awt.Color(0, 0, 0));
         jLabel1.setText("CATAT YUKK");
 
-<<<<<<< HEAD
-=======
         jLabel2.setIcon(new javax.swing.ImageIcon("C:\\Users\\ASUS\\Downloads\\icons8-money-100.png")); // NOI18N
 
->>>>>>> 06e84b9a7b2297b86d475ace60d913531175a731
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
         jPanel3Layout.setHorizontalGroup(
@@ -280,15 +277,15 @@ public class viewLogin extends javax.swing.JFrame {
     }//GEN-LAST:event_btnMasukKeyPressed
 
     private void txtUsernameKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtUsernameKeyPressed
-            if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
-            btnMasukActionPerformed(null);
-        }
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
+        btnMasukActionPerformed(null);
+    }
     }//GEN-LAST:event_txtUsernameKeyPressed
 
     private void txtPasswordKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtPasswordKeyPressed
-            if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
-            btnMasukActionPerformed(null);
-        }
+        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
+        btnMasukActionPerformed(null);
+    }
     }//GEN-LAST:event_txtPasswordKeyPressed
 
     /**
@@ -320,13 +317,6 @@ public class viewLogin extends javax.swing.JFrame {
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
-<<<<<<< HEAD
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-=======
->>>>>>> 06e84b9a7b2297b86d475ace60d913531175a731
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
